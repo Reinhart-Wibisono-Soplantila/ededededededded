1f5c7051f8aa6985193fe10d8c64f1515996fc33
Reinhart Wibisono Soplantila <reinhartsoplantila@gmail.com>
2021-04-27 21:41:42 +0800

    listview retrofit tanpa viewmodel done

--- app/src/main/java/id/ac/unhas/infocovid19/ui/provinsi/ProvinsiListFragment.kt
+++ app/src/main/java/id/ac/unhas/infocovid19/ui/provinsi/ProvinsiListFragment.kt
@@ -9,8 +9,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import id.ac.unhas.infocovid19.BuildConfig
 import id.ac.unhas.infocovid19.R
+import id.ac.unhas.infocovid19.model.DataProvinsi
 import id.ac.unhas.infocovid19.model.DataSource
+import id.ac.unhas.infocovid19.network.ApiEndPoint
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ProvinsiListFragment : Fragment() {
 
@@ -35,6 +43,36 @@ class ProvinsiListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //objek listview
+        listView = view.findViewById(R.id.list_view_provinsi)
+
+        //buat objek retrofit
+        val builder = Retrofit.Builder()
+            .baseUrl("https://api.kawalcorona.com/")//from gradle : "https://api.kawalcorona.com/"
+            .addConverterFactory(GsonConverterFactory.create())
+        val retrofit = builder.build()
+
+        var apiEndpoint = retrofit.create(ApiEndPoint::class.java)
+
+        apiEndpoint.getDataProvinsi().enqueue(object : Callback<DataProvinsi>{
+            override fun onFailure(call: Call<DataProvinsi>, t: Throwable) {
+                Log.e("Network Error","Error: ${t.message}")
+            }
+
+            override fun onResponse(call: Call<DataProvinsi>, response: Response<DataProvinsi>) {
+                val dataProvinsi = response.body()
+                Log.d("Debugging",dataProvinsi.toString());
+                val adapter = context?.let{
+                    ArrayAdapter(it, R.layout.listview_item_provinsi,
+                        toListOfStrings(dataProvinsi))
+                }
+
+                listView.adapter = adapter
+            }
+
+        })
+
+        /*
         val jsonList:String =
             activity?.applicationContext?.let {
                 DataSource.getJsonDataFromAsset(it, "provinsi.json")
@@ -48,18 +86,16 @@ class ProvinsiListFragment : Fragment() {
 
         viewModel.getProvinsisFromRepo()
 
+         */
+
         Log.d("MainFragment","createView")
-        listView = view.findViewById(R.id.list_view_provinsi)
+    }
+    private fun toListOfStrings(dataProvinsi: DataProvinsi?): Array<String?> {
+        val listItems = arrayOfNulls<String>(dataProvinsi?.size ?: 0)
 
-        val listItems = arrayOfNulls<String>(viewModel.provinsis.size)
-
-        viewModel.provinsis.forEachIndexed { index, provinsi ->
+        dataProvinsi?.forEachIndexed { index, provinsi ->
             listItems[index] = provinsi.toString()
         }
-
-        val adapter = context?.let { ArrayAdapter(it,R.layout.listview_item_provinsi,listItems) }
-
-        listView.adapter = adapter
+        return listItems;
     }
-
 }
